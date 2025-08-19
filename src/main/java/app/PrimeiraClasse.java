11a288b6e4228cc24d7b7d886c727f6cad5b3b3c
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-08-19 01:37:39 +0000

    Conceito de encapsulamento - 18/08/2025

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,39 @@ package app;
 
 public class PrimeiraClasse{
     public static void main(String[] args){
-        System.out.println("Olá Mundo!");
+        Aluno a1 = new Aluno();
+        a1.nome = "Robson";
+        a1.idade = 32;
+
+        Aluno a2 = a1;
+        a2.nome = "Wertin";
+        a2.idade = 46;
+
+        System.out.println("nome = " + a1.nome);
+        System.out.println("idade = " + a1.idade);
+
+        System.out.println("---------------------");
+        
+        System.out.println("nome = " + a2.nome);
+        System.out.println("idade = " + a2.idade);
+
+        System.out.println("---------------------");
+
+        System.out.println("[ CHAMADA DE METÓDO ]");
+        System.out.println("A1 = " + a1.getDados());
+        System.out.println("A2 = " + a2.getDados());
+
+        System.out.println("---------------------");
+
+        Aluno a3 = new Aluno("Maria", 35);
+        System.out.println("[ CHAMADA DE METÓDO ]");
+        System.out.println("A3 = " + a3.getDados());
+
+        System.out.println("---------------------");
+
+        Cliente c1 = new Cliente();
+        c1.setNome("Cliente");
+        System.out.println("[ CHAMADA DE GET ]");
+        System.out.println(c1.getNome());
     }
 }
